fb1bcae7624d6b0c07d8810e8486bcf083beb5d0
Rakhymzhan Nurgissa <@Rakhymzhan_N>
2023-04-17 15:55:25 +0600

    feat: added Material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { RegistrationListComponent } from './registration-list/registration-list
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { NgToastModule } from 'ng-angular-popup';
 import { NgConfirmModule } from 'ng-confirm-box';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +23,9 @@ import { NgConfirmModule } from 'ng-confirm-box';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
     NgToastModule,
     NgConfirmModule,
   ],
